d3a561672109772d21fbfbd76ecfba36c95b9297
ahsan-codejit <ahsan@codejit.com>
2017-06-10 01:51:59 +0200

    updates

--- app.js
+++ app.js
@@ -10,29 +10,15 @@ if(options) {
 		winston.info(i++, '. Calling....')
 		testerObj.run()
 		.then((res)=>{
-			console.log(i+'. res: ', res);
+			// console.log(ri+'. res: ', res);
 			ri++;
-			if(ri>=8) clearInterval(waitingInterval);
 		})
 		.catch((err)=>{
-			console.log('err: ', err);
+			// console.log(ri+'. err: ', err);
 			ri++;
-			if(ri>=8) clearInterval(waitingInterval);
 		});
 		if(i>=9) clearInterval(runnerInterval);
-		// var waitingInterval = setInterval(function(){
-		// 	console.log('Waiting.......');
-		// }, 1000);
-		// .then((result)=>{
-		// 	winston.debug('success');
-		// 	if(ri++ >= 8) clearInterval(runnerInterval);
-		// 	return result;
-		// });
 	}, 100);
-	// setTimeout(function(){
-	// 	winston.info('Interval is being closed');
-	// 	clearInterval(runnerInterval);
-	// },1000);
 } else {
 	winston.warn('config option is not set.');
 }
--- tester.js
+++ tester.js
@@ -12,12 +12,13 @@ module.exports = class Tester {
 		winston.info('runner has been called');
 		return request(this.options)
 	    .then((body) => {
-	       	winston.debug('response body:', body)
+	       	winston.debug('response body:', (typeof body==='object')?JSON.stringify(body):body);
+	       	return body;
 	    })
 	    .catch((err)=>{
 	    	var error = err;
 	    	if(err.response&&err.response.body) error = err.response.body;
-	    	winston.error('Error: ', error.toString());
+	    	winston.error('Error: ', (typeof error==='object')?JSON.stringify(error):error);
 	    	return error;
 	        // POST failed... 
 	    });
